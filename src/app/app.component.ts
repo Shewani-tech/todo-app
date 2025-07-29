2422d64c254fe512c7f1de4773ffe537c7154a90
Rawat <ashish.rawat@sap.com>
2025-07-29 14:39:32 +0530

    added changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Todo';
+  currentRoute='';
+  title = 'todo-list';
+  
+  constructor(private router: Router) {
+    // Watch for route changes
+
+    this.router.events.subscribe(() => {
+    this.currentRoute = this.router.url;
+    });
+  }
+  navigate(link: string) {
+    this.router.navigate([`/${link}`]);
+  }
 }
---
+++ src/app/components/todo-form/todo-form.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { TodoService } from '../../service/todo.service';
+import { FormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-todo-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './todo-form.component.html',
+  styleUrl: './todo-form.component.scss'
+})
+export class TodoFormComponent {
+ todo = '';
+
+  constructor(public todoService: TodoService,private toasterService: ToastrService) {
+      console.log('coming here');
+   }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(item:string){
+    this.todoService.addTodo(this.todo);
+      this.toasterService.success(`Todo ${item} Added!`, 'Added Successfuly');
+    this.todo='';
+    
+  }
+}
